73ffe7bcdbce68fd6d6d5f894798f483128d6161
Anatoy Nosov <jomeisama@gmail.com>
2018-09-08 11:19:03 +0300

    Sing in works

--- api/lib/api_web/router.ex
+++ api/lib/api_web/router.ex
@@ -1,8 +1,14 @@
 defmodule ApiWeb.Router do
   use ApiWeb, :router
 
+  alias ApiWeb.Guardian
+
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  pipeline :jwt_authenticated do
+    plug Guardian.AuthPipeline
   end
 
   scope "/api", ApiWeb do
@@ -14,5 +20,11 @@ defmodule ApiWeb.Router do
 
     post "/sign_up", UserController, :create
     post "/sign_in", UserController, :sign_in
+  end
+
+  scope "/api/v1", ApiWeb do
+    pipe_through [:api, :jwt_authenticated]
+
+    get "/my_user", UserController, :show
   end
 end
